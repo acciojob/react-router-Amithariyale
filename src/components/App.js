4201c98f1d914beb4675c1a2862d01d7361c2da6
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-03-02 01:59:39 +0530

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,16 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+        <BrouserRouter>
+          <Routes>
+            <Route path="" element={<h1>Welcome to my website!</h1>}/>
+            <Route path="about" element={<div><h1>About</h1><p>This is a simple React Router program.</p></div>}/>
+          </Routes>
+        </BrouserRouter>
     </div>
   )
 }
